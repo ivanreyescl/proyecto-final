807fe33024b5e0ab72dfa52450430f58c50d0f2c
OrlandoRam <eorcastro@gmail.com>
2025-08-18 21:08:19 -0600

    correccion register login

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -29,7 +29,17 @@ export const loginUser = async (req, res) => {
     if (!user) {
       return res.status(401).json({ error: 'Credenciales inválidas' });
     }
-    res.json({ message: 'Inicio de sesión exitoso', user });
+
+    const userFrontend = {
+      id: user.id,
+      email: user.email,
+      firstName: user.first_name, // <-- mapeo correcto
+      lastName: user.last_name,   // <-- mapeo correcto
+      role_description: user.role_description || '',
+      // agrega otros campos si necesitas
+    };
+
+    res.json({ message: 'Inicio de sesión exitoso', user: userFrontend });
   } catch (error) {
     console.error('Error =>', error);
     res.status(500).json({ error: 'Error al procesar la solicitud' });
